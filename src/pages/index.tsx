d20865e3091fda39255dedad05c4034158334a93
GuiTh <guilhermetcarlos@hotmail.com>
2022-10-05 23:58:11 -0300

    att crud

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,13 +1,24 @@
 import Layout from "../components/Layout"
+import Tabela from "../components/Tabela"
+import Cliente from "../core/Cliente"
 
 export default function Home() {
+
+  const clientes = [
+    new Cliente('Gustavo', 30, "1"),
+    new Cliente('Guilherme', 22, "2"),
+    new Cliente('Karol', 22, "3"),
+    new Cliente('Karla', 50, "4")
+  ]
+
+
   return (
     <div className={`
     flex justify-center items-center h-screen 
     bg-gradient-to-r from-blue-500 to-purple-500 text-white
     `}>
       <Layout titulo="Cadastro Simples">
-        <span>Conteudo</span>
+        <Tabela clientes={clientes}></Tabela>
       </Layout>
     </div>
   )
