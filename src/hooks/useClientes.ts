b7bb07b4dcc49ece030bd05808a780eaeaf5c37d
GuiTh <guilhermetcarlos@hotmail.com>
2022-10-13 13:06:17 -0300

    fim do crud

---
+++ src/hooks/useClientes.ts
@@ -0,0 +1,52 @@
+import Cliente from '../core/Cliente'
+import ColecaoCliente from '../backend/db/ColecaoCliente'
+import ClienteRepositorio from '../core/ClienteRepositorio'
+import { useState, useEffect } from 'react'
+import useTabelaOuForm from './useTabelaOuForm'
+
+//ESSE HOOK TRABALHA COM AS FUNCOES DE ALTERAR O FORMULARIO
+
+export default function useCliente(){
+
+    const repo: ClienteRepositorio = new ColecaoCliente()
+    const { tabelaVisivel, formularioVisivel, exibirFormulario, exibirTabela} = useTabelaOuForm()
+    const [cliente, setCliente] = useState<Cliente>(Cliente.vazio())
+    const [clientes, setClientes] = useState<Cliente[]>()
+  
+    useEffect(obterTodos, [])
+  
+    function obterTodos(){
+      repo.obterTodos().then(clientes =>{
+        setClientes(clientes)
+        exibirTabela()
+      })
+    }
+    function selecionarCliente(cliente:Cliente){
+      setCliente(cliente)
+      exibirFormulario()
+    }
+    async  function excluirCliente(cliente: Cliente){
+      await repo.excluir(cliente)
+      obterTodos()
+    }
+     async function salvarCliente(cliente: Cliente){
+      await repo.salvar(cliente)
+      obterTodos()
+    }
+    function novoCliente(cliente: Cliente){
+      setCliente(Cliente.vazio())
+      exibirFormulario()
+    }
+
+    return{
+        tabelaVisivel,
+        cliente,
+        clientes,
+        exibirTabela,
+        novoCliente,
+        excluirCliente,
+        selecionarCliente,
+        salvarCliente,
+    }
+
+}
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -3,45 +3,30 @@ import Layout from "../components/Layout"
 import Tabela from "../components/Tabela"
 import Cliente from "../core/Cliente"
 import Formulario from "../components/Formulario"
-import { useState } from "react"
+import { useEffect, useState } from "react"
+import ClienteRepositorio from "../core/ClienteRepositorio"
+import ColecaoCliente from "../backend/db/ColecaoCliente"
+import useCliente from '../hooks/useClientes'
 
 export default function Home() {
 
-
-  const clientes = [
-    new Cliente('Gustavo', 30, "1"),
-    new Cliente('Guilherme', 22, "2"),
-    new Cliente('Karol', 22, "3"),
-    new Cliente('Karla', 50, "4")
-  ]
-
-
-  function clienteSelecionado(cliente:Cliente){
-    setCliente(cliente)
-    setVisivel('form')
-  }
-  function clienteExcluido(cliente: Cliente){
-    console.log(`Excluir...${cliente.nome}`)
-  }
-  function salvarCliente(cliente: Cliente){
-    setVisivel('tabela')
-  }
-  function novoCliente(cliente: Cliente){
-    setCliente(Cliente.vazio())
-    setVisivel('form')
-  }
-
-  const [cliente, setCliente] = useState<Cliente>(Cliente.vazio())
-  const [visivel, setVisivel] = useState<'tabela' | 'form'>('tabela')
-
-
+  const { 
+    tabelaVisivel,
+    excluirCliente,
+    selecionarCliente,
+    salvarCliente,
+    novoCliente,
+    exibirTabela,
+    cliente,
+    clientes } = useCliente()
+ 
   return (
     <div className={`
     flex justify-center items-center h-screen 
     bg-gradient-to-r from-blue-500 to-purple-500 text-white
     `}>
       <Layout titulo="Cadastro Simples">
-        {visivel === 'tabela' ? (
+        {tabelaVisivel ? (
           <>
           <div className="flex justify-end">
 
@@ -53,11 +38,14 @@ export default function Home() {
           </div>
 
           <Tabela clientes={clientes}
-          clienteSelecionado={clienteSelecionado}
-          clienteExcluido={clienteExcluido}></Tabela>
+          clienteSelecionado={selecionarCliente}
+          clienteExcluido={excluirCliente}></Tabela>
             </>    
         ):(
-            <Formulario cliente={cliente} clienteMudou={salvarCliente} cancelado={() => setVisivel('tabela')}></Formulario>
+            <Formulario 
+            cliente={cliente}
+             clienteMudou={salvarCliente}
+              cancelado={exibirTabela}/>
           )}
       </Layout>
     </div>
